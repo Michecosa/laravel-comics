4128d2fb9e681286f0cc84e80d4fe8cc45a2b2c9
Michela <micheladellagatta1@gmail.com>
2026-02-10 14:49:37 +0100

    feat: display comics list from configuration file

---
+++ resources/views/components/comics-cards.blade.php
@@ -0,0 +1,6 @@
+<div class="card h-100"> 
+    <img src="{{$thumb}}" alt="{{$title}} image" class="card-img-top">
+    <div class="card-body">
+        <h3 class="card-title">{{$title}}</h3>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.main')
+  
+@php
+  $comics = config("comics");
+  # dd($comics);
+@endphp
+
 
 @section('contenuto')
   <h1>Laravel Comics</h1>
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4"> 
+
+      @foreach ($comics as $comic)
+      <div class="col">
+        <x-comics-cards>
+          <x-slot:thumb>{{$comic['thumb']}}</x-slot:thumb>
+          <x-slot:title>{{$comic['title']}}</x-slot:title>
+        </x-comics-cards>
+      </div>          
+      @endforeach
+
+    </div>
 @endsection
