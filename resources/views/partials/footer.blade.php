f6a67d4bf00187f93e8cac4045fa148ec7bee3f7
Michela <micheladellagatta1@gmail.com>
2026-02-10 17:31:29 +0100

    feat: add footer structure

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,76 @@
 <footer>
-  <hr>
-  Footer
+  <div class="footer-main">
+    <div class="container py-5 d-flex justify-content-between align-items-start position-relative overflow-hidden">
+        
+      <div class="footer-links d-flex gap-5">
+        <div>
+          <h5 class="text-white text-uppercase fw-bold mb-3">DC Comics</h5>
+          <ul class="list-unstyled">
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+
+          <h5 class="text-white text-uppercase fw-bold mb-3 mt-4">Shop</h5>
+          <ul class="list-unstyled">
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h5 class="text-white text-uppercase fw-bold mb-3">DC</h5>
+          <ul class="list-unstyled">
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h5 class="text-white text-uppercase fw-bold mb-3">Sites</h5>
+          <ul class="list-unstyled">
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="footer-logo-bg">
+        <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="DC Background">
+      </div>
+
+    </div>
+  </div>
+
+  <div class="footer-social py-4 bg-dark-custom">
+    <div class="container d-flex justify-content-between align-items-center">
+        
+      <button class="btn btn-outline-primary text-white border-2 px-3 py-2 fw-bold">
+        SIGN-UP NOW!
+      </button>
+
+      <div class="d-flex align-items-center gap-3">
+        <span class="text-primary fw-bold text-uppercase">Follow Us</span>
+        <div class="social-icons d-flex gap-2">
+          <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="FB">
+          <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="TW">
+          <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="YT">
+          <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="PI">
+          <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="PE">
+        </div>
+      </div>
+
+    </div>
+  </div>
 </footer>
